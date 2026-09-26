5a5974e88a586cf1c21832f915b94c12dc5dd1eb
r_maganinho <113379890+QuicoCat@users.noreply.github.com>
2026-09-26 16:14:13 +0100

    Publish client spell faction hotfixes for all mount spells

--- src/server/game/Entities/Player/CollectionMgr.cpp
+++ src/server/game/Entities/Player/CollectionMgr.cpp
@@ -87,24 +87,27 @@ void CollectionMgr::LoadMountDefinitions()
     TC_LOG_INFO("server.loading", ">> Published {} test Mount.Flags hotfix for mount 1267",
         factionFlagTestHotfix.GetAppliedHotfixesCount());
 
-    // Test the client's spell faction flags on one collected mount whose
-    // Mount.Flags hotfix hid it from the journal instead of making it usable.
-    DB2HotfixGenerator<SpellMiscEntry> factionSpellTestHotfix(sSpellMiscStore);
+    // SpellMisc faction attributes also prevent the client from summoning
+    // collected mounts. Only change spells referenced by Mount.db2 records.
+    std::unordered_set<uint32> mountSpells;
+    for (MountEntry const* mount : sMountStore)
+        if (mount->SourceSpellID > 0)
+            mountSpells.insert(uint32(mount->SourceSpellID));
+
+    DB2HotfixGenerator<SpellMiscEntry> factionSpellHotfixes(sSpellMiscStore);
     for (SpellMiscEntry const* misc : sSpellMiscStore)
     {
-        if (misc->SpellID != 61230)
+        if (!mountSpells.contains(misc->SpellID)
+            || !(misc->Attributes[7] & (SPELL_ATTR7_HORDE_SPECIFIC_SPELL | SPELL_ATTR7_ALLIANCE_SPECIFIC_SPELL)))
             continue;
 
-        TC_LOG_INFO("server.loading", "Mount spell 61230: SpellMisc ID={}, Attributes7={}",
-            misc->ID, misc->Attributes[7]);
-        if (misc->Attributes[7] & (SPELL_ATTR7_HORDE_SPECIFIC_SPELL | SPELL_ATTR7_ALLIANCE_SPECIFIC_SPELL))
-            factionSpellTestHotfix.ApplyHotfix(misc->ID, [](SpellMiscEntry* entry)
-            {
-                entry->Attributes[7] &= ~(SPELL_ATTR7_HORDE_SPECIFIC_SPELL | SPELL_ATTR7_ALLIANCE_SPECIFIC_SPELL);
-            }, true);
+        factionSpellHotfixes.ApplyHotfix(misc->ID, [](SpellMiscEntry* entry)
+        {
+            entry->Attributes[7] &= ~(SPELL_ATTR7_HORDE_SPECIFIC_SPELL | SPELL_ATTR7_ALLIANCE_SPECIFIC_SPELL);
+        }, true);
     }
-    TC_LOG_INFO("server.loading", ">> Published {} test mount spell faction hotfixes",
-        factionSpellTestHotfix.GetAppliedHotfixesCount());
+    TC_LOG_INFO("server.loading", ">> Published {} unrestricted faction mount spell hotfixes",
+        factionSpellHotfixes.GetAppliedHotfixesCount());
 
     QueryResult result = WorldDatabase.Query("SELECT spellId, otherFactionSpellId FROM mount_definitions");
 
